c6fc7009f092124f833038c8ab51bd67ad0c3bf3
Bianca <protonrock@zoho.com>
2023-12-20 16:46:50 +0100

    Fixing ratios js

--- ja_interactive.js
+++ ja_interactive.js
@@ -1,25 +1,43 @@
+// declara as variáveis
+let canvasSize;
+let canvasX;
+let canvasY;
+let firstSize;
+let firstX;
+let firstY;
+let secondSize;
+let secondX;
+let secondY;
+
 function setup() {
   createCanvas(windowWidth, windowHeight); //Tamanho do canvas = tamanho da janela
 
-  //Tamanho do primeiro quadrado (80%) do que for menor, largura ou algura
-  firstSize = Math.min(windowWidth, windowHeight)*0.6;
+  //Tamanho do primeiro quadrado (90%) do que for menor, largura ou altura
+  canvasSize = Math.min(windowWidth, windowHeight) * 0.9;
 
-  //centralizar o primeiro quadrado (diferenca entre o tamanho do canvas e o tamanho do primeiro quadrado / 2)
-  firstX = (windowWidth-firstSize)/2;
+  //centralizar o primeiro quadrado (diferença entre o tamanho do canvas e o tamanho do primeiro quadrado / 2)
+  canvasX = (windowWidth - canvasSize) / 2;
 
-  // distancia pra baixo eh 6% to tamanho do primeiro quadrado
-  firstY = (windowHeight-firstSize)-firstSize*0.16;
+  canvasY = 0;
+
+   //Tamanho do primeiro quadrado (60%) do que for menor, largura ou altura
+  firstSize = canvasSize * 0.6;
 
 
-  //Tamanho do segundo quadrado (50%) do que for menor, altura ou largura
-  secondSize = Math.min(windowWidth, windowHeight)*0.4;
+  //centralizar o primeiro quadrado (diferença entre o tamanho do canvas e o tamanho do primeiro quadrado / 2)
+  firstX = (windowWidth - firstSize) / 2;
 
-  //centralizar o segundo quadrado (diferenca entre o tamanho do canvas e do segundo quadrado / 2)
-  secondX = (windowWidth-secondSize)/2;
+  //distancia pra baixo entre canvas e primeiro quadrado
+  firstY = (canvasSize - firstSize) - firstSize * 0.14;
 
-  // distancia pra baixo eh 20% to tamanho do segundo quadrado
-  secondY = (windowHeight-secondSize)-secondSize*0.36;
+  //Tamanho do segundo quadrado (40%) do que for menor, altura ou largura
+  secondSize = canvasSize * 0.4;
 
+  //centralizar o segundo quadrado (diferença entre o tamanho do canvas e do segundo quadrado / 2)
+  secondX = (windowWidth - secondSize) / 2;
+
+  //distancia pra baixo é 34% do tamanho do segundo quadrado
+  secondY = (canvasSize - secondSize) - secondSize * 0.34;
 }
 
 function draw() {
@@ -27,33 +45,15 @@ function draw() {
   // Constrain mouseY to the range [0, height]
   let constrainedMouseY = constrain(mouseY, 0, height);
 
-  background(map(constrainedMouseY, 0, height, 0, 255), 131, 191);
+  background(255);
 
   // Adjusting fill color based on constrained mouseY
+  fill(map(constrainedMouseY, 0, height, 0, 255), 131, 191);
+  square(canvasX, canvasY, canvasSize);
 
   fill(196, map(constrainedMouseY, 0, height, 0, 255) - 51, 0);
-  square(firstX,firstY, firstSize);
+  square(firstX, firstY, firstSize);
 
   fill(243, map(constrainedMouseY, 0, height, 0, 255) - 58, 0);
-  square(secondX,secondY, secondSize);
+  square(secondX, secondY, secondSize);
 }
-
-/* Static, without MouseY interaction
-let firstDistance = 90;
-let secondDistance = 40;
-let firstSquareY = 150;
-let secondSquareY = 250;
-
-function setup() {
-  createCanvas(850, 850);
-  background(55, 132, 193);
-}
-
-function draw() {
-  noStroke();
-  fill(196, 51, 0);
-  square(150, firstSquareY + (150 - firstDistance), 550);
-  fill(243, 58, 0);
-  square(250, secondSquareY + ([250 - firstDistance] - secondDistance), 350);
-}
-*/
